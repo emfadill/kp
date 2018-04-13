afba5fa35d08e56f214c33ffb33e86e9349a592d
unknown <mramdan437@gmail.com>
2018-04-13 23:07:21 +0700

    ada yang salah di judul dashboard mhs

--- application/views/dashboard_mhs.php
+++ application/views/dashboard_mhs.php
@@ -29,7 +29,7 @@ desired effect
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Bimbingan</b>Online</span>
+      <span class="logo-lg"><b>Bimbingan</b>Proposal</span>
     </a>
 
     <!-- Header Navbar -->
